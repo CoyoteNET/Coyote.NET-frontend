5e11375130eb3c2135458765c5e9a2a383dbaaca
dotNETSanta <45518533+dotNETSanta@users.noreply.github.com>
2019-08-15 23:29:26 +0200

    Basic functionalities - creating account, signing in, opening threads, loading threads.

--- CoyoteFrontend/Data/Service.cs
+++ CoyoteFrontend/Data/Service.cs
@@ -1,26 +1,80 @@
+using System;
 using Newtonsoft.Json;
 using System.Net.Http;
 using System.Threading.Tasks;
-using CoyoteNETCore.Shared.RequestInput;
 using System.Text;
+using Microsoft.Extensions.Configuration;
+using CoyoteNETCore.Shared.ResultHandling;
+using System.Net.Http.Headers;
 
 namespace CoyoteFrontend.Data
 {
     public class Service
-    {        
-        public async Task<bool> TryRegister(RegisterInput command)
+    {
+        private readonly IConfiguration _configuration;
+
+        public Service(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<Result<U>> TryPOST<T, U>(T command, string endpoint, string bearer_token = "")
         {
             using (var client = new HttpClient())
             {
                 var json = JsonConvert.SerializeObject(command);
 
-                var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Post, "http://localhost:5000/Account/Register/")
+                var request = new HttpRequestMessage(HttpMethod.Post, _configuration["Backend:Host"] + endpoint)
                 {
                     Content = new StringContent(json, Encoding.UTF8, "application/json")
-                });
+                };
 
-                var responseJson = await response.Content.ReadAsStringAsync();
-                return true; // JsonConvert.DeserializeObject<bool>(responseJson);
+                if (!string.IsNullOrEmpty(bearer_token))
+                {
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearer_token);
+                }
+
+                var response = await client.SendAsync(request);
+
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.Clear();
+                    Console.WriteLine(responseString);
+                    Console.WriteLine(typeof(U));
+                    var t = JsonConvert.DeserializeObject<U>(responseString);
+                    return new Result<U>(t);
+                }
+
+                return new Result<U>(ErrorType.BadRequest, responseString);
+            }
+        }
+
+        public async Task<Result<U>> TryGET<U>(string endpoint, string bearer_token = "")
+        {
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, _configuration["Backend:Host"] + endpoint)
+                {
+                };
+
+                if (!string.IsNullOrEmpty(bearer_token))
+                {
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearer_token);
+                }
+
+                var response = await client.SendAsync(request);
+
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var t = JsonConvert.DeserializeObject<U>(responseString, new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore });
+                    return new Result<U>(t);
+                }
+
+                return new Result<U>(ErrorType.BadRequest, responseString);
             }
         }
     }
---
+++ CoyoteFrontend/FakeLocalStorage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoyoteFrontend
+{
+    public static class FakeLocalStorage
+    {
+        // temporary work around to hold JWT somewhere
+
+        public static string Username { get; set; }
+
+        public static string Token { get; set; }
+
+        public static DateTime ExpiresAt { get; set; }
+    }
+}
